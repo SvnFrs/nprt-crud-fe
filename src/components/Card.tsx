39a53a0650edbec9567a1d98904b88b1a2597673
SvnFrs <thaidvq.work@gmail.com>
2024-11-01 01:28:43 +0700

    monolithic fe first

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,65 +1,82 @@
 /* eslint-disable react/react-in-jsx-scope */
+import LaunchOutlinedIcon from "@mui/icons-material/LaunchOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBulletedOutlined";
+
 export default function Card() {
   return (
     <>
-      <div className="overflow-x-auto">
-        <table className="w-full text-sm text-center rtl:text-right border-b-2 border-black">
-          <thead className="bg-base text-pink uppercase">
-            <tr>
-              <th className="px-3 py-5 hover:bg-overlay0 hover:text-maroon">
-                ID
-              </th>
-              <th className="px-3 py-5 hover:bg-overlay0 hover:text-maroon">
-                Name
-              </th>
-              <th className="px-3 py-5 hover:bg-overlay0 hover:text-maroon">
-                Price
-              </th>
-              <th className="px-3 py-5 hover:bg-overlay0 hover:text-maroon">
-                Date
-              </th>
-              <th className="px-3 py-5"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="">
-              <th className="px-6 py-4 font-medium whitespace-nowrap text-black">
-                1
-              </th>
-              <td>Thai</td>
-              <td>20</td>
-              <td>2024</td>
-              <td>Edit</td>
-            </tr>
-            <tr className="">
-              <th className="px-6 py-4 font-medium whitespace-nowrap text-black">
-                1
-              </th>
-              <td>Thai</td>
-              <td>20</td>
-              <td>2024</td>
-              <td>Edit</td>
-            </tr>
-            <tr className="">
-              <th className="px-6 py-4 font-medium whitespace-nowrap text-black">
-                1
-              </th>
-              <td>Thai</td>
-              <td>20</td>
-              <td>2024</td>
-              <td>Edit</td>
-            </tr>
-            <tr className="">
-              <th className="px-6 py-4 font-medium whitespace-nowrap text-black">
-                1
-              </th>
-              <td>Thai</td>
-              <td>20</td>
-              <td>2024</td>
-              <td>Edit</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="overflow-x-auto lg:flex lg:flex-col items-center bg-base py-16">
+        <div className="lg:w-1/2 xs:w-full py-5">
+          <AddBoxOutlinedIcon style={{ color: "#cdd6f4" }} fontSize="large" />
+          <div className="px-6">
+            <div className="font-bold text-2xl text-text">Add Items</div>
+
+            <div className="w-full bg-text rounded-md my-5 h-24 pt-4 px-5">
+              <div className="flex flex-row">
+                <div className="w-5/12">
+                  <div className="font-sans text-lg text-crust">Key</div>
+                  <div className="font-sans text-lg text-crust">
+                    <input
+                      className="w-full bg-text border-l-4 border-black focus:outline-none h-8 px-3"
+                      placeholder="Grapes"
+                    />
+                  </div>
+                </div>
+                <div className="w-6/12">
+                  <div className="font-sans text-lg text-crust">Value</div>
+                  <div className="font-sans text-lg text-crust">
+                    <input
+                      className="w-full bg-text border-l-4 border-black focus:outline-none h-8 px-3"
+                      placeholder="0"
+                    />
+                  </div>
+                </div>
+                <div className="w-1/12 py-5">
+                  <LaunchOutlinedIcon
+                    style={{ color: "#11111b" }}
+                    fontSize="large"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="lg:w-1/2 xs:w-full py-5">
+          <FormatListBulletedOutlinedIcon
+            style={{ color: "#cdd6f4" }}
+            fontSize="large"
+          />
+          <div className="px-6">
+            <div className="font-bold text-2xl text-text">List Items</div>
+
+            <div className="w-full rounded-md my-5 h-40">
+              <div className="grid grid-cols-5 gap-4 justify-items-center align-middle">
+                <div className="h-36 w-36 border-4 rounded-3xl text-center p-8 border-rosewater">
+                  <div className="text-center text-text text-xl">Grapes</div>
+                  <div className="text-center text-text text-3xl font-extrabold">10</div>
+                </div>
+                <div className="h-36 w-36 border-4 rounded-3xl text-center p-8 border-flamingo">
+                  <div className="text-center text-text text-xl">Grapes</div>
+                  <div className="text-center text-text text-3xl font-extrabold">10</div>
+                </div>
+                <div className="h-36 w-36 border-4 rounded-3xl text-center p-8 border-pink">
+                  <div className="text-center text-text text-xl">Grapes</div>
+                  <div className="text-center text-text text-3xl font-extrabold">10</div>
+                </div>
+                <div className="h-36 w-36 border-4 rounded-3xl text-center p-8 border-mauve">
+                  <div className="text-center text-text text-xl">Grapes</div>
+                  <div className="text-center text-text text-3xl font-extrabold">10</div>
+                </div>
+                <div className="h-36 w-36 border-4 rounded-3xl text-center p-8 border-red">
+                  <div className="text-center text-text text-xl">Grapes</div>
+                  <div className="text-center text-text text-3xl font-extrabold">10</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-/** @type {import('tailwindcss').Config} */
+import catppuccinTailwind from "@catppuccin/tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@catppuccin/tailwindcss")({
-    // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
-    // default is `false`, which means no prefix
-    // prefix: "ctp",
-    // which flavour of colours to use by default, in the `:root`
-    // defaultFlavour: "latte",
-  }),],
+  plugins: [catppuccinTailwind({
+    // your plugin configuration
+  })],
 };
